eebff8ffcedc93f5de3e01011a42ba580b5e31b3
Martin E. Zulliger <exe@thepeak.de>
2017-06-30 18:08:28 +0200

    Added support for image and font loading

--- webpack.config.js
+++ webpack.config.js
@@ -33,6 +33,30 @@ module.exports = {
           'localIdentName=[hash:base64:5]',
           'sass-loader',
         ],
+      },
+      {
+        test: /\.png$/,
+        loader: 'url-loader?limit=100000',
+      },
+      {
+        test: /\.jpg$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=application/octet-stream',
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: 'url-loader?limit=10000&mimetype=image/svg+xml',
       }
     ]
   },
