b92f2d441ed520a912d220247e722f96ec79f224
Martin E. Zulliger <exe@thepeak.de>
2018-02-18 13:44:33 +0100

    Fixed eslint aliases warnings

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
     "extends": "airbnb",
     "rules": {
       "react/jsx-filename-extension": 0,
+      "react/forbid-prop-types": 0,
       "no-underscore-dangle": 0,
       "func-names": 0,
       "one-var": ["error", {
@@ -9,5 +10,10 @@ module.exports = {
         "initialized": "never"
       }],
       "one-var-declaration-per-line": 0
+    },
+    "settings": {
+      "import/resolver": {
+        "webpack": {}
+      }
     }
 };
